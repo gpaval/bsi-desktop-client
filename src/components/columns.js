4024a43637cdcc62bf32061c900af6960a125c63
Adriana <adrianaschipor7@gmail.com>
2020-11-12 17:54:54 +0200

    "TableComponent wip"

---
+++ src/components/columns.js
@@ -0,0 +1,18 @@
+export const COLUMNS = [
+  {
+    Header: "Name",
+    accessor: "name",
+  },
+  {
+    Header: "ID",
+    accessor: "id",
+  },
+  {
+    Header: "R/W Permission",
+    accessor: "permission",
+  },
+  {
+    Header: " ",
+    accessor: "info",
+  },
+];

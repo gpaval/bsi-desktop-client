ca6fcddd76c9a8c267879b3ea9874bf3c3b2c674
Andrei <andrei.vasilache@assist.ro>
2020-12-07 08:55:59 +0200

    edit cms functionality

--- src/pages/CmsUpdate/CmsUpdate.js
+++ src/pages/CmsUpdate/CmsUpdate.js
@@ -3,27 +3,37 @@ import { useHistory, useParams } from "react-router-dom";
 
 import CmsComponent from "../../components/Cms/CmsComponent";
 import StyledCmsUpdate from "./StyledCmsUpdate";
+import axios from "axios";
 
 const CmsUpdate = () => {
   const { id } = useParams();
   const history = useHistory();
 
   const [name, setName] = useState("");
+  const [extraData, setExtraData] = useState("");
   const [items, setItems] = useState([]);
 
   useEffect(() => {
-    fetch(`${process.env.REACT_APP_ENDPOINT}/getThirdParty?id=${id}`)
+    fetch(`${process.env.REACT_APP_ENDPOINT}/getAllRequiredKeys`)
       .then((rawData) => rawData.json())
-      .then((data) => {
-        const newItems = data.requiredKeys.map((item) => ({
-          name: item,
-          isSelected: false,
-        }));
+      .then((allKeys) => {
+        fetch(`${process.env.REACT_APP_ENDPOINT}/getThirdParty?id=${id}`)
+          .then((rawData) => rawData.json())
+          .then((data) => {
+            const newItems = allKeys.map((item) => ({
+              name: item,
+              isSelected: data.requiredKeys.includes(item),
+            }));
 
-        setItems(newItems);
-        setName(data.name);
+            setExtraData({
+              id: data.id,
+              managerEmail: data.managerEmail,
+            });
+            setItems(newItems);
+            setName(data.name);
+          });
       });
-  }, []);
+  }, [id]);
 
   const onToggleItem = (index) => {
     const newItems = [...items];
@@ -32,10 +42,25 @@ const CmsUpdate = () => {
   };
 
   const onSubmit = () => {
-    const selectedItems = items
+    const requiredKeys = items
       .filter((item) => item.isSelected)
       .map((filteredItems) => filteredItems.name);
-    console.log({ selectedItems });
+
+    axios
+      .post(`${process.env.REACT_APP_ENDPOINT}/editThirdParty`, {
+        id: extraData.id,
+        managerEmail: extraData.managerEmail,
+        name,
+        requiredKeys,
+      })
+      .then((data) => {
+        history.goBack();
+        console.log({ data });
+      })
+      .catch((err) => {
+        console.log({ err });
+      });
+
     console.log("submitted!");
   };
   const onCancel = () => {
